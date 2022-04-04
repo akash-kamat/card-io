749844d159d7ce400ba878856410837f601f81b7
akash kamat <akash.kamat.10@gmail.com>
2022-04-04 18:41:31 +0530

    fixed positioning of divs

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,13 +4,6 @@ import io from 'socket.io-client'
 
 const socket = io("http://127.0.0.1:3002");
 function CardList({ users, user, setUser, setUsers, loggedIn, clicks, setClicks }) {
-    
-    const userList = users.sort((a, b) => (a.clicks < b.clicks ? 1 : -1))
-    // const userList = users.sort(function(a, b) {
-    //     var c = b.clicks-a.clicks;
-    //     if (c) return c;
-    //     return Date.parse(b.joinedOn)-Date.parse(a.joinedOn);
-    //   });
 
     return (
         <div className="cardlist">
